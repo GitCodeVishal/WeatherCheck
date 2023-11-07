00b4e525641b163d8375b74d4eda44d96a3faa91
GitCodeVishal <vkashyap1998@gmail.com>
2023-11-07 20:54:14 +0530

    Added the change icon feature

--- script.js
+++ script.js
@@ -4,16 +4,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const searchBox = document.querySelector(".search input");
     const searchButton = document.querySelector(".search button");
+    const weatherIcon = document.querySelector(".weather-icon");
 
     const checkWeather = async (city) => {
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-        var data = await response.json();
+        const data = await response.json();
         console.log(data);
 
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        // Check weather condition and set the weather icon
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
     }
 
     searchButton.addEventListener("click", () => {
